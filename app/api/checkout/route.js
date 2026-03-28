af89082948a7e34b08d2898697c2b1c6353c8074
treasurehubsolutions <treasurehubsolutions@gmail.com>
2026-03-28 18:06:47 -0400

    fix all dynamic routes

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,4 +1,5 @@
 export const dynamic = 'force-dynamic'
+
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,4 +1,5 @@
 export const dynamic = 'force-dynamic'
+
 import { getAdminClient } from '../../../lib/supabase'
 
 export async function POST(req) {
